584f3894cd98251ed439230d9bb8634530332899
Muhammad Furqan Sheikh <furqansh.2oo6@gmail.com>
2026-04-18 19:32:19 +0500

    feat(os_layer): Add umbrella header files for integration in os_layer

---
+++ src/os_layer/ipc/include/ipc_module.h
@@ -0,0 +1,4 @@
+/**
+ * @note This is the umbrella header file that will integrate export oll the ipc files to the os_layer.h
+ *
+ */
---
+++ src/os_layer/scheduler/include/scheduler_module.h
@@ -0,0 +1,4 @@
+/**
+ * @note This is the umbrella file that will integrate export oll the ipc files to the os_layer.h
+ *
+ */
