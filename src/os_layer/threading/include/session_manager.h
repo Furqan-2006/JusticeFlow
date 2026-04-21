1df42ccc6452d3de7f83e3e9727ca702cac18d51
Muhammad Furqan Sheikh <furqansh.2oo6@gmail.com>
2026-04-21 20:26:49 +0500

    refactor: Update synchronization classes and implement RAII for Mutex and Semaphore

--- src/os_layer/threading/include/session_manager.h
+++ src/os_layer/threading/include/session_manager.h
@@ -1,10 +1,8 @@
-#ifndef SESSION_MANAGER_H
-#define SESSION_MANAGER_H
+#pragma once
 
 #include <unordered_map>
 #include "sync.h"
 
-// Define a basic SessionContext struct 
 struct SessionContext {
     int officer_id;
     int socket_fd;
@@ -16,41 +14,15 @@ private:
     std::unordered_map<int, SessionContext> registry;
     Mutex registry_mutex;
 
-    // Singleton setup
-    SessionManager() {}
+    SessionManager();
     SessionManager(const SessionManager&) = delete;
     SessionManager& operator=(const SessionManager&) = delete;
 
 public:
-    static SessionManager& getInstance() {
-        static SessionManager instance;
-        return instance;
-    }
+    static SessionManager& getInstance();
 
-    void register_session(int thread_id, SessionContext context) {
-        MutexGuard lock(registry_mutex); // Locks the map safely!
-        registry[thread_id] = context;
-    } // lock goes out of scope here and automatically unlocks!
-
-    void unregister_session(int thread_id) {
-        MutexGuard lock(registry_mutex);
-        registry.erase(thread_id);
-    }
-
-    bool getSession(int thread_id, SessionContext& out_context) {
-        MutexGuard lock(registry_mutex);
-        auto it = registry.find(thread_id);
-        if (it != registry.end()) {
-            out_context = it->second;
-            return true;
-        }
-        return false;
-    }
-
-    int getActiveCount() {
-        MutexGuard lock(registry_mutex);
-        return registry.size();
-    }
+    void register_session(int thread_id, SessionContext context);
+    void unregister_session(int thread_id);
+    bool getSession(int thread_id, SessionContext& out_context);
+    int getActiveCount();
 };
-
-#endif // SESSION_MANAGER_H
