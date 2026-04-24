9fe2852303c78fb62893c723df8aef4a77d7e556
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2026-04-24 09:29:18 +0500

    furqan dear please instruct your claude to not write his  bak bak in my files, i think we're done for the os-layer

--- src/os_layer/threading/include/threading_module.h
+++ src/os_layer/threading/include/threading_module.h
@@ -1,4 +1,18 @@
 /**
  * @note This is the umbrella header file that will integrate export oll the ipc files to the os_layer.h
+ 
+ FURQAN MERI FILE MEI APNI BAK BAK NA LIKHO 
+ #IDISAPPROVE#NOCONSENT#ABUBAKAR'SLIVESMATTER
  *
  */
+ 
+ #ifndef THREADING_MODULE_H
+#define THREADING_MODULE_H
+
+#include "sync.h"
+#include "connection_gate.h"
+#include "session_manager.h"
+#include "worker.h"
+#include "thread_pool.h"
+
+#endif // THREADING_MODULE_H
