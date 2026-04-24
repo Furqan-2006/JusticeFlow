a955412d64d45fc761cb177c33d6a0036fe8c6a1
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2026-04-24 09:31:40 +0500

    Apparently we were not done

---
+++ src/os_layer/threading/include/mlock_guard.h
@@ -0,0 +1,19 @@
+#ifndef MLOCK_GUARD_H
+#define MLOCK_GUARD_H
+
+#include <stddef.h>
+
+class mlock_guard {
+private:
+    void* addr;
+    size_t len;
+public:
+    mlock_guard(void* address, size_t size);
+    ~mlock_guard();
+    
+    // Prevent copying to avoid double-unocking or zeroing early
+    mlock_guard(const mlock_guard&) = delete;
+    mlock_guard& operator=(const mlock_guard&) = delete;
+};
+
+#endif // MLOCK_GUARD_H
